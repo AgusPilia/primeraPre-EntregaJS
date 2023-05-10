a155c1ee23504eb051ed36984bbcf7c21894cd79
AgusPilia <piliaagustin@gmail.com>
2023-05-09 23:09:27 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+const num1 = parseFloat(prompt('Ingrese el primer número:'));
+const num2 = parseFloat(prompt('Ingrese el segundo número:'));
+
+const operacion = prompt('Seleccione la operación: suma (+), resta (-), multiplicación (*) o división (/)');
+
+let resultado;
+
+
+if (operacion === '+') {
+    resultado = num1 + num2;
+    alert(`El resultado es: ${resultado}`);
+} else if (operacion === '-') {
+    resultado = num1 - num2;
+    alert(`El resultado es: ${resultado}`);
+} else if (operacion === '*') {
+    resultado = num1 * num2;
+    alert(`El resultado es: ${resultado}`);
+} else if (operacion === '/') {
+    resultado = num1 / num2;
+    alert(`El resultado es: ${resultado}`);
+} else {
+    alert('Operación inválida');
+}
